5a884adffcb55f92fb47448db2ad68affd046247
Shivang Yadav <shivangyadav121@gmail.com>
2023-07-03 23:14:19 +0530

    api added

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
-export default function MovieItem() {
+
+export default function MovieItem(props) {
+    
   return (
-    <div>MovieItem</div>
+    <>
+    
+    <div className='container my-5'>
+   <div className="card col-md-4 bg-dark text-light" style={{width: '300px'}}>
+  <img src={props.image} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{props.title }</h5>
+    <p className="card-text">{props.overview}</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  
+</div>
+
+    </>
   )
 }
---
+++ src/components/Movies.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import MovieItem from './MovieItem';
+
+export default function Movies() {
+  const [movies, setMovies] = useState(null);
+
+  const update = async () => {
+    const url = "https://api.themoviedb.org/3/movie/top_rated?api_key=5751fdb0570f52a040bda8aa291614b9&page=2";
+    let data = await fetch(url);
+    let parseData = await data.json();
+    setMovies(parseData.results);
+  }
+
+  useEffect(() => {
+    update();
+  }, []);
+
+  return (
+    <>
+      <h1 className='text-center text-light'>Top Rated Movies</h1>
+      <div className='container'>
+        <div className="row">
+          {movies ? (
+            movies.map((element) => (
+              <div className="col-md-4" key={element.id}>
+                <MovieItem
+                  title={element.original_title ? element.original_title : ""}
+                  overview={element.overview ? element.overview : ""}
+                  image={`https://image.tmdb.org/t/p/w220_and_h330_face${element.poster_path}`}
+                />
+              </div>
+            ))
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      </div>
+    </>
+  )
+}
