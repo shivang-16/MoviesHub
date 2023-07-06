d6f89f1f9f46de904ab6f5a83d4ec3d80b09bc92
Shivang Yadav <shivangyadav121@gmail.com>
2023-07-06 18:55:57 +0530

    navbar optimise footer added and button function

---
+++ src/components/Footer.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <main id="main-body">
+      <footer>
+        <div className="footer-content">
+          <h3>
+            The site is uses the{" "}
+            <a
+              href="https://developer.themoviedb.org/docs"
+              target="_blank"
+              className="myName"
+            >
+              TMBD API
+            </a>{" "}
+            to fetch the data
+          </h3>
+          <h4>
+            Created by{" "}
+            <a
+              href="https://twitter.com/16_shivang"
+              target="_blank"
+              className="myName"
+            >
+              Shivang
+            </a>
+          </h4>
+          <p>Connect with me -</p>
+          <div className="SocialMedia">
+            <a
+              href="https://www.linkedin.com/in/shivang-yadav-b83979257/"
+              target="_blank"
+            >
+              <div className="socialBox linkedin"></div>
+            </a>
+            <a href="https://twitter.com/16_shivang" target="_blank">
+              <div className="socialBox twitter"></div>
+            </a>
+            <a href="https://github.com/shivang-16" target="_blank">
+              <div className="socialBox github"></div>
+            </a>
+            <a href="https://www.threads.net/@shivang_18" target="_blank">
+              <div className="socialBox thread"></div>
+            </a>
+            <a href="https://www.instagram.com/shivang_18/" target="_blank">
+              <div className="socialBox instagram"></div>
+            </a>
+          </div>
+          <div className="footer-line">
+            <p>
+              &copy; {new Date().getFullYear()} Shivang. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
+    </main>
+  );
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React ,  {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
+  
+  const [activeNavItem, setActiveNavItem] = useState('Home');
+
+  const handleOnClickNavItem = (navItem) => {
+    setActiveNavItem(navItem);
+  };
   let burgerClick = document.querySelector('.BurgerClick')
   
   const handleOnClickBurger=()=>{
@@ -23,13 +29,37 @@ export default function Navbar() {
       <Link to="/"><h1>Movies<span className='hub'>Hub</span></h1></Link>
      </div>
      <div className="nav-items-box">
-      <Link className="nav-item-1 nav-item" to="/">Home</Link>
-      <Link className="nav-item-2 nav-item" to="/movies">Movies</Link>
-      <Link className="nav-item-3 nav-item" to="/tvshows">TV Shows</Link>
-      <Link className="nav-item-4 nav-item" to="/series">Series</Link>
-     </div>
+            <Link
+              className={`nav-item-1 nav-item ${activeNavItem === 'Home' ? 'nav-item-active' : ''}`}
+              to="/"
+              onClick={() => handleOnClickNavItem('Home')}
+            >
+              Home
+            </Link>
+            <Link
+              className={`nav-item-2 nav-item ${activeNavItem === 'Movies' ? 'nav-item-active' : ''}`}
+              to="/movies"
+              onClick={() => handleOnClickNavItem('Movies')}
+            >
+              Movies
+            </Link>
+            <Link
+              className={`nav-item-3 nav-item ${activeNavItem === 'TV Shows' ? 'nav-item-active' : ''}`}
+              to="/tvshows"
+              onClick={() => handleOnClickNavItem('TV Shows')}
+            >
+              TV Shows
+            </Link>
+            <Link
+              className={`nav-item-4 nav-item ${activeNavItem === 'Series' ? 'nav-item-active' : ''}`}
+              to="/series"
+              onClick={() => handleOnClickNavItem('Series')}
+            >
+              Series
+            </Link>
+          </div>
+        </div>
     
-     </div>
      <div className='burger' onClick={handleOnClickBurger}>
       <div className="line"></div>
       <div className="line"></div>
